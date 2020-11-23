638bf24871fe10569415f2b9fb843f73855e4f44
Dmar <71910379+MeDmar950@users.noreply.github.com>
2020-11-23 21:36:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,63 +5,11 @@ const Guild = require('./models/guild');
 const antilinks = require('./models/antilinks');
 const prefix = '$';
 client.on('ready', async() => console.log('Ready'));
-mongoose.connect('mongodb+srv://work:work123@cluster0.e0df5.mongodb.net/work?retryWrites=true&w=majority', {
+mongoose.connect('MongoDBLink', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 });
-
-client.on('message', async(message) => {
-    if(message.author.bot) return;
-    let args = message.content.split(' ');
-    if(args[0].toLowerCase() === `${prefix}warn`) {
-    let user = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-    if(!user) return message.channel.send(`**:x: | Usage: ${prefix}warn <@User> <Reason>**`);
-    Guild.findOne({Guild: message.guild.id, User: user.user.id}, (err, doc) => {
-        if(err) console.error(err);
-        if(!doc) {
-            let newWarns = new Guild({
-                User: user.user.id,
-                Guild: message.guild.id,
-                Warns: [
-                    {
-                        Moderator: message.author.id,
-                        Reason: args.slice(2).join(' ')
-                    }
-                ]
-            })
-            newWarns.save();
-            message.channel.send(`**Done Has Warn ${user}**`);
-        } else {
-            doc.Warns.unshift({
-                Moderator: message.author.id,
-                Reason: args.slice(2).join(' ')
-            })
-            doc.save();
-            message.channel.send(`**Done Has Warn ${user}**`);
-        }
-    })
-}    
-})
-        
-client.on('message', async(message) => {
-    if(message.author.bot) return;
-    if(message.content.startsWith(prefix + 'warns')) {
-    let args = message.content.split(' ');
-    let user = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-    if(!user) return message.channel.send(`**:x: | Usage: ${prefix}warn <@User> <Reason>**`);
-    Guild.find({Guild: message.guild.id, User: user.user.id}, async(err, data) => {
-        if(err) console.error(err);
-        if(!data.length) return message.channel.send(`**Not Has Any Warn**`);
-        let embed = new Discord.MessageEmbed().setColor('#36393e').setTitle(`${user.user.tag} Warns on ${message.guild.name}`).setDescription(data.map(d => {
-            return d.Warns.map((w, i) => `**#${i+1}**: Moderator: ${message.guild.members.cache.get(w.Moderator).user.tag}, Reason: ${w.Reason}`).join("\n")
-        }))
-        message.channel.send(embed)
-        
-    })
-}    
-});
-
 
 
 client.on('message', async(message) => {
@@ -126,4 +74,4 @@ function links(str) {
   }
 
 
-client.login('NzYwNTI1MzI4NDUwOTc3Nzky.X3NUaA.SxvMxjhDsaiTcmtheMRoqKMyd9w')
+client.login('TOKEN HERE');
