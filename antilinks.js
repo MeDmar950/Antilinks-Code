96f48b6f2085877513e44161ab850eb8d1e4d5a3
Dmar <71910379+MeDmar950@users.noreply.github.com>
2020-11-23 21:35:22 +0200

    Delete antilinks.js

--- antilinks.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const Antilinks = new mongoose.Schema({
-    Guild: String,
-    run: String
-});
-
-module.exports = mongoose.model("Antilinks", Antilinks);
