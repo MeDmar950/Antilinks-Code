e5b5dc0e085429cc9385eafe10df30489f4a829e
Dmar <71910379+MeDmar950@users.noreply.github.com>
2020-11-23 21:35:11 +0200

    Update antilinks.js

--- models/antilinks.js
+++ models/antilinks.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
 
+const Antilinks = new mongoose.Schema({
+    Guild: String,
+    run: String
+});
+
+module.exports = mongoose.model("Antilinks", Antilinks);
